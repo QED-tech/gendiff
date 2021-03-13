82b82e1379998ab88ee2181512b3b54ef00593da
Vladislav <qedirast@gmail.com>
2021-03-13 22:01:02 +0700

    refactor tests

--- src/Differ.php
+++ src/Differ.php
@@ -3,39 +3,35 @@
 namespace Differ;
 
 use Exception;
-
 use function Differ\Formatters\getPlain;
 use function Differ\parser;
 
 function gendiff(string $firstFile, string $secondFile): string
 {
     try {
-        filesIsExists($firstFile, $secondFile);
+        filesIsExists([$firstFile, $secondFile]);
     } catch (Exception $e) {
         return $e->getMessage();
     }
 
     [$firstAsArray, $secondAsArray] = getFilesAsSortArray($firstFile, $secondFile);
-    $diff = getDiff($firstAsArray, $secondAsArray);
-    return getPlain($diff);
+    return getDiff($firstAsArray, $secondAsArray);
 }
 
-function getDiff(array $firstList, array $secondList): array
+function getDiff(array $firstList, array $secondList): string
 {
     $keys = array_unique(array_merge(array_keys($firstList), array_keys($secondList)));
     $diff = array_map(function ($key) use ($firstList, $secondList) {
         return parser($key, $firstList, $secondList);
     }, $keys);
-    return array_values($diff);
+    return getPlain(array_values($diff));
 }
 
-function filesIsExists(string $firstFile, string $secondFile): void
+function filesIsExists(array $files): void
 {
-    if (!file_exists($firstFile)) {
-        throw new Exception(sprintf("%s file not exists", $firstFile));
-    }
-
-    if (!file_exists($secondFile)) {
-        throw new Exception(sprintf("%s file not exists", $firstFile));
+    foreach ($files as $file) {
+        if (!file_exists($file)) {
+            throw new Exception(sprintf("%s file not exists", $file));
+        }
     }
 }
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -1,8 +1,7 @@
 <?php
 namespace Differ\ParserTests;
 
-use function Differ\getDiff;
-use function Differ\getFilesAsSortArray;
+use function Differ\gendiff;
 use PHPUnit\Framework\TestCase;
 
 class DifferTest extends TestCase
@@ -10,21 +9,19 @@ class DifferTest extends TestCase
     /**
      * @dataProvider additionalProvider
      */
-    public function testGetDiffJson(string $firstFilePath, string $secondFilePath)
+    public function testGetDiff(string $firstFilePath, string $secondFilePath, $expected)
     {
-        $expected = include __DIR__ . '/fixtures/expected.php';
-        [$firstAsArray, $secondAsArray] = getFilesAsSortArray($firstFilePath, $secondFilePath);
-        $actual = getDiff($firstAsArray, $secondAsArray);
-
-        $this->assertEquals($expected[0], $actual);
+        $actual = gendiff($firstFilePath, $secondFilePath);
+        $this->assertEquals($expected, $actual);
     }
 
     public function additionalProvider()
     {
         $path = __DIR__ . '/fixtures/data';
+        $expected = include __DIR__ . '/fixtures/expected.php';
         return [
-            ["{$path}/file1.json", "{$path}/file2.json"],
-            ["{$path}/file1.yml", "{$path}/file2.yml"],
+            ["{$path}/file1.json", "{$path}/file2.json", $expected[0][0]],
+            ["{$path}/file1.yml", "{$path}/file2.yml", $expected[0][0]],
         ];
     }
 }
