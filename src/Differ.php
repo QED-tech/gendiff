8036ef9d0809fcdc8570ebde06023979e3f6b4d3
Vladislav <qedirast@gmail.com>
2021-03-15 12:15:35 +0700

    refactor

--- src/Differ.php
+++ src/Differ.php
@@ -2,20 +2,11 @@
 
 namespace Differ\Differ;
 
-use Exception;
-
 use function Differ\Differ\Formatters\plain;
 use function Differ\Differ\Formatters\stylish;
-use function Differ\Differ\parser;
 
 function genDiff(string $firstFile, string $secondFile, string $format = 'stylish'): string
 {
-    try {
-        filesIsExists([$firstFile, $secondFile]);
-    } catch (Exception $e) {
-        return $e->getMessage();
-    }
-
     [$firstAsArray, $secondAsArray] = getFiles($firstFile, $secondFile);
     return formatted($format, getDiff($firstAsArray, $secondAsArray));
 }
@@ -24,17 +15,75 @@ function getDiff(array $firstList, array $secondList): array
 {
     $keys = getAllUniqueKeys($firstList, $secondList);
     $diff = array_map(function ($key) use ($firstList, $secondList) {
-        if (is_array($firstList[$key] ?? null) && is_array($secondList[$key] ?? null)) {
+        $valueFirst = boolToString($firstList[$key] ?? null);
+        $valueSecond = boolToString($secondList[$key] ?? null);
+        if (is_array($valueFirst) && is_array($valueSecond)) {
             return [
                 'key' => $key,
                 'description' => 'parent',
-                'children' => getDiff($firstList[$key], $secondList[$key]),
+                'children' => getDiff($valueFirst, $valueSecond),
             ];
         }
-        return parser($key, $firstList, $secondList);
+
+        if (!array_key_exists($key, $secondList)) {
+            return [
+                'key' => $key,
+                'value' => $valueFirst,
+                'changed' => true,
+                'description' => 'deleted',
+            ];
+        }
+
+        if (!array_key_exists($key, $firstList)) {
+            return [
+                'key' => $key,
+                'value' => $valueSecond,
+                'changed' => true,
+                'description' => 'added',
+            ];
+        }
+
+        if ($valueFirst !== $valueSecond) {
+            return [
+                'key' => $key,
+                'oldValue' => $valueFirst,
+                'newValue' => $valueSecond,
+                'changed' => true,
+                'description' => 'update',
+            ];
+        }
+
+        return [
+            'key' => $key,
+            'value' => $valueFirst,
+            'changed' => false,
+            'description' => 'unchanged',
+        ];
     }, $keys);
 
     return array_values($diff);
+}
+
+/**
+ *
+ * @param mixed $value
+ * @return mixed
+ */
+function boolToString($value)
+{
+    if ($value === false) {
+        return 'false';
+    }
+
+    if ($value === true) {
+        return 'true';
+    }
+
+    if ($value === null) {
+        return 'null';
+    }
+
+    return $value;
 }
 
 function getAllUniqueKeys(array $firstList, array $secondList): array
@@ -49,7 +98,7 @@ function getAllUniqueKeys(array $firstList, array $secondList): array
     return $keys;
 }
 
-function formatted(string $format, $diff): string
+function formatted(string $format, array $diff): string
 {
     switch ($format) {
         case 'stylish':
@@ -60,17 +109,5 @@ function formatted(string $format, $diff): string
             return json_encode($diff, JSON_PRETTY_PRINT);
         default:
             return trim(stylish($diff));
-    }
-}
-
-/**
- * @throws Exception
- */
-function filesIsExists(array $files): void
-{
-    foreach ($files as $file) {
-        if (!file_exists($file)) {
-            throw new Exception(sprintf("%s file not exists", $file));
-        }
     }
 }
